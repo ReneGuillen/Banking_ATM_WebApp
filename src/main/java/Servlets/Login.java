48baae3f1a1b6001334ec057c14103dbb8ba3d0d
ReneGuillen <62676755+ReneGuillen@users.noreply.github.com>
2021-04-07 11:19:49 -0500

    Updated Get/Post Requests

--- src/main/java/Servlets/Login.java
+++ src/main/java/Servlets/Login.java
@@ -12,13 +12,16 @@ import java.sql.SQLException;
 @WebServlet(name = "Login", urlPatterns = {"/login"})
 public class Login extends HttpServlet {
 
+    //Method to verify user's information store in database if exists to login.
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //Make sure all fields are entered before checking the database.
         if(isRequestEmpty(request)){
             request.setAttribute("message", "Unsuccessful Login, Please make sure you enter all information required");
             RequestDispatcher requestDispatcher = request.getRequestDispatcher("index.jsp");
             requestDispatcher.forward(request, response);
         }
         else {
+            //check database to make sure all information is correct.
             String username = request.getParameter("userName");
             String password = request.getParameter("password");
             Member member = new Member(username, password);
@@ -40,10 +43,12 @@ public class Login extends HttpServlet {
         }
     }
 
+    //Get request to check user's information from database.
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         doPost(request, response);
     }
-
+    
+    //Method to check if fields are empty or not.
     protected boolean isRequestEmpty(HttpServletRequest request){
         String user = request.getParameter("userName");
         String pass = request.getParameter("password");
